ab44d58a311953a5c00a144cc8bfad38ec065f9b
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-29 21:18:48 +0300

    task 4 in progress

---
+++ src/src/db/models/contacts.js
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: email,
+      required: optional,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const contactsCollection = model('contacts', contactsSchema);
