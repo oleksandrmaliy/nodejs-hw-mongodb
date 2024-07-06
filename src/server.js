7ee23c2bd293703d2c3cc45cd21a9654bd26f0ba
Oleksandr <avmaliy71@gmail.com>
2024-07-06 08:20:56 +0300

    Again bugs fixed

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -35,6 +36,7 @@ export const setupServer = () => {
   });
 
   app.use('/uploads/', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
